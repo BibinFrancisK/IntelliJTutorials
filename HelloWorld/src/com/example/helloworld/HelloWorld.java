b0bc578d08c3ab942578929afdb58ed1e51e001f
BibinFrancis <bibinfrancis404@outlook.com>
2022-06-17 22:36:46 +0530

    first commit

---
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World from IntelliJ!");
+
+        /*
+            Alt+ins : create new Java class
+            We can type fully qualified class name - IntelliJ will autogenerate the packages if it doesn't exists
+            Shift+ent : new line
+            Ctrl+J : see live templates
+            Esc : close templates/ pop-ups
+            Ctrl+. : select first template and place a "." after it
+            Class files are in /out
+            Production code is in /out/production
+            Building JAR:
+                        - File --> Project Structure --> Artifacts --> New --> JAR --> From Modules
+                        - Build --> Build Artifacts
+            Run configurations:
+                        - Shift(2) --> Run Config --> + --> JAR App --> Path to JAR
+         */
+
+    }
+}
